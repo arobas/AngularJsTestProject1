c29148f965fff60ad2c0326f6f86ccf9e41a3a83
arobas <robas.a@gmail.com>
2016-04-25 11:51:29 +0200

    Added files via upload

--- src/app.js
+++ src/app.js
@@ -1,1 +1,25 @@
+angular.module('app', []);
 
+angular.module('app').directive('numberToMonth', function(){
+  
+    return {
+        restrict: 'AE',
+        scope: {
+        body: '@input'
+        },
+        template: '<p> Število: {{ body }}'
+    };
+    
+});
+angular.module('app').controller('MainController', function($scope){
+
+    
+    });
+      
+angular.module('app').filter('monthName', [function() {
+    return function (monthNumber) { //1 = January
+        var monthNames = [ 'Januar', 'Februar', 'Marec', 'April', 'Maj', 'Junij',
+            'Julij', 'A0vgust', 'September', 'Oktober', 'November', 'December' ];
+        return monthNames[monthNumber - 1];
+    }
+}]);
